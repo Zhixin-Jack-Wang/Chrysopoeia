232bc9ec9d8b80fb8a7e971190d817a5d6c2a72a
Zhixin Wang <harima.ky@gmail.com>
2019-08-13 17:02:04 -0400

    minor changes

--- client/src/pages/Welcome/Welcome.js
+++ client/src/pages/Welcome/Welcome.js
@@ -1,59 +1,68 @@
 import React, { Component } from "react";
-import Axios from 'axios';
+import Axios from "axios";
 import { Link, Route, Redirect } from "react-router-dom";
-import logo from '../../assets/Icon.png';
-import styled from 'styled-components';
+import logo from "../../assets/Icon.png";
+import styled from "styled-components";
 
 class Dashboard extends Component {
-
-    render(){
+  render() {
     return (
-        <>
-         <DivWrapper>
-            <div className="row mt-5">
-                <div className="col-md-6 m-auto">
-                <div className="card card-body text-center">
-                    <div className="logo-wrapper">
-                        <img src={logo} className="logo"/>
-                    </div>
-                    <h4>Welcome to Chrysopoeia</h4>
-                    <p>Create an account or login</p>
-                    <div className="anchor-wrapper">
-                    <a href="/users/register" className="btn btn-secondary btn-block buttons">
-                        Register
-                    </a>
-                    <a href="/users/login" className="btn btn-secondary btn-block buttons">Login</a>
-                    </div>
+      <>
+        <DivWrapper>
+          <div className="row mt-5">
+            <div className="col-md-6 m-auto">
+              <div className="card card-body text-center">
+                <div className="logo-wrapper">
+                  <img src={logo} className="logo" />
                 </div>
+                <h4>Welcome to Chrysopoeia</h4>
+                <p>Create an account or login</p>
+                <div className="anchor-wrapper">
+                  <a
+                    href="/users/register"
+                    className="btn btn-secondary btn-block buttons"
+                  >
+                    Register
+                  </a>
+                  <a
+                    href="/users/login"
+                    className="btn btn-secondary btn-block buttons login-btn"
+                  >
+                    Login
+                  </a>
                 </div>
+              </div>
             </div>
-            </DivWrapper>
-        </>
-    )
+          </div>
+        </DivWrapper>
+      </>
+    );
   }
 }
 
 export default Dashboard;
-const DivWrapper = styled.div
-`
-.logo-wrapper{
-    width:100%;
-    display:flex;
-    justify-content:center;
-}
-.logo{
-    width:8rem;
-}
+const DivWrapper = styled.div`
+  .logo-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+  .logo {
+    width: 8rem;
+  }
 
-.buttons{
-    width:50%;
-    background-color:var(--lightBlue);
-    border:none;
-}
-.anchor-wrapper{
-    width:100%;
-    display:flex;
-    flex-direction:column;
-    align-items:center;
-}
-`
+  .buttons {
+    width: 50%;
+    background-color: var(--lightBlue);
+    border: none;
+  }
+  .anchor-wrapper {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .login-btn {
+    margin-bottom: 3rem;
+  }
+`;
